4a8dce3d3875ac423aa89a3d0c426a57a598b339
Marci <hajdumarcel@gmail.com>
2023-04-04 16:47:41 +0200

    Make a Route and Link to it

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My blog</title>
+    <link rel="stylesheet" href="app.css">
+</head>
+<body>
+    <article>
+        <h1> <a href="/post"> My first post</a></h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, necessitatibus odio. Sit blanditiis eveniet, natus sint quaerat, aut, inventore repudiandae quos nisi iste tempora iusto? Fugiat sapiente blanditiis inventore repellat.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente necessitatibus sunt facere dicta praesentium doloremque quo quis impedit, nesciunt tenetur dolorum illo deleniti, dignissimos tempore quam, ut harum? Possimus, eveniet.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post"> My second post</a></h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, necessitatibus odio. Sit blanditiis eveniet, natus sint quaerat, aut, inventore repudiandae quos nisi iste tempora iusto? Fugiat sapiente blanditiis inventore repellat.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente necessitatibus sunt facere dicta praesentium doloremque quo quis impedit, nesciunt tenetur dolorum illo deleniti, dignissimos tempore quam, ut harum? Possimus, eveniet.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post"> My third post</a></h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, necessitatibus odio. Sit blanditiis eveniet, natus sint quaerat, aut, inventore repudiandae quos nisi iste tempora iusto? Fugiat sapiente blanditiis inventore repellat.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente necessitatibus sunt facere dicta praesentium doloremque quo quis impedit, nesciunt tenetur dolorum illo deleniti, dignissimos tempore quam, ut harum? Possimus, eveniet.
+        </p>
+    </article>
+</body>
+</html>
