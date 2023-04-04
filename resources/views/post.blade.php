7b765dadbc3895580de8fb48c4b2a22bb4efe283
Marci <hajdumarcel@gmail.com>
2023-04-04 17:37:51 +0200

    Store Blog Posts as  HTML Files

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,8 +1,16 @@
-<article>
-    <h1><a href="/post"> My first post</a></h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, necessitatibus odio. Sit blanditiis eveniet, natus sint quaerat, aut, inventore repudiandae quos nisi iste tempora iusto? Fugiat sapiente blanditiis inventore repellat.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente necessitatibus sunt facere dicta praesentium doloremque quo quis impedit, nesciunt tenetur dolorum illo deleniti, dignissimos tempore quam, ut harum? Possimus, eveniet.
-    </p>
-    <a href="/"> Go Back</a>
-</article>
+<!DOCTYPE html>
+
+<title>My Blog</title>
+
+<link rel="stylesheet" href="app.css">
+
+<body>
+    <article>
+        <?= $post; ?>
+    </article>
+
+    <a href="/">Go Back</a>
+    
+</body>
+
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,21 +9,21 @@
 </head>
 <body>
     <article>
-        <h1> <a href="/post"> My first post</a></h1>
+        <h1> <a href="/posts/my-first-post"> My first post</a></h1>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, necessitatibus odio. Sit blanditiis eveniet, natus sint quaerat, aut, inventore repudiandae quos nisi iste tempora iusto? Fugiat sapiente blanditiis inventore repellat.
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente necessitatibus sunt facere dicta praesentium doloremque quo quis impedit, nesciunt tenetur dolorum illo deleniti, dignissimos tempore quam, ut harum? Possimus, eveniet.
         </p>
     </article>
 
     <article>
-        <h1><a href="/post"> My second post</a></h1>
+        <h1><a href="/posts/my-second-post"> My second post</a></h1>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, necessitatibus odio. Sit blanditiis eveniet, natus sint quaerat, aut, inventore repudiandae quos nisi iste tempora iusto? Fugiat sapiente blanditiis inventore repellat.
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente necessitatibus sunt facere dicta praesentium doloremque quo quis impedit, nesciunt tenetur dolorum illo deleniti, dignissimos tempore quam, ut harum? Possimus, eveniet.
         </p>
     </article>
 
     <article>
-        <h1><a href="/post"> My third post</a></h1>
+        <h1><a href="/posts/my-third-post"> My third post</a></h1>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, necessitatibus odio. Sit blanditiis eveniet, natus sint quaerat, aut, inventore repudiandae quos nisi iste tempora iusto? Fugiat sapiente blanditiis inventore repellat.
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente necessitatibus sunt facere dicta praesentium doloremque quo quis impedit, nesciunt tenetur dolorum illo deleniti, dignissimos tempore quam, ut harum? Possimus, eveniet.
         </p>
